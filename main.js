f4a46693e86e31c9e9b40330bd69b74f7af4e451
Svetlana Smirnova <apelsinka2002sveta@gmail.com>
2023-01-13 15:47:37 +0300

    1

---
+++ main.js
@@ -0,0 +1,30 @@
+const calc = document.querySelector('.calc');
+const calc__output = document.querySelector('.calc__output');
+
+calc.addEventListener('click', function(e){
+    if(!e.target.classList.contains('calc__btn')) return;
+    const value = e.target.innerText;
+    let result = [];
+    switch(value){
+        case 'C':
+            calc__output.innerText = '';
+        break;
+
+        case '<':
+            calc__output.innerText = calc__output.innerText.substring(0, calc__output.innerText.length - 1);
+        break;
+
+        case '=':
+            if(calc__output.innerText.includes('/0')){
+                calc__output.innerText = 'на ноль делить нельзя!';
+            }else{
+                calc__output.innerText = eval(calc__output.innerText).toFixed(3);
+            }
+        break;
+
+
+        default:
+            calc__output.innerText += value;
+    }
+
+});
